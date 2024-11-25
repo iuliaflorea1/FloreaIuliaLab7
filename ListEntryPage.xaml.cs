00844984c44e6b3c0b9e9c64757876707625132a
iuliaflorea1 <iulia@IULIA_FLOREA>
2024-11-25 15:10:19 +0200

    changes

---
+++ ListEntryPage.xaml.cs
@@ -0,0 +1,37 @@
+using FloreaIuliaLab7.Models;
+
+namespace FloreaIuliaLab7;
+
+public partial class ListEntryPage : ContentPage
+{
+	public ListEntryPage()
+	{
+		InitializeComponent();
+	}
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        myListView.ItemsSource = await App.Database.GetShopListsAsync();
+    }
+
+    async void OnShopListAddedClicked(object sender, EventArgs e)
+    {
+        await Navigation.PushAsync(new ListPage
+        {
+            BindingContext = new ShopList()
+        });
+    }
+
+    async void OnListViewItemSelected(object sender, SelectedItemChangedEventArgs e)
+    {
+        if (e.SelectedItem != null)
+        {
+            await Navigation.PushAsync(new ListPage
+            {
+                BindingContext = e.SelectedItem as ShopList
+            });
+        }
+
+    }
+}
