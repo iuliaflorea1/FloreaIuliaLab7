97cf7bec7fe1436bb72f5c92a8e35f31fa989d06
iuliaflorea1 <iulia@IULIA_FLOREA>
2024-12-07 16:05:02 +0200

    ChangesthEHEH

--- Data/ShoppingListDatabase.cs
+++ Data/ShoppingListDatabase.cs
@@ -9,14 +9,16 @@ using FloreaIuliaLab7.Models;
 
 namespace FloreaIuliaLab7.Data
 {
-    public class ShoppingListDatabase
+    public class ShopListDatabase
     {
         readonly SQLiteAsyncConnection _database;
 
-        public ShoppingListDatabase(string dbPath)
+        public ShopListDatabase(string dbPath)
         {
             _database = new SQLiteAsyncConnection(dbPath);
             _database.CreateTableAsync<ShopList>().Wait();
+            _database.CreateTableAsync<Product>().Wait();
+            _database.CreateTableAsync<ListProduct>().Wait();
         }
 
         public Task<List<ShopList>> GetShopListsAsync()
@@ -46,6 +48,48 @@ namespace FloreaIuliaLab7.Data
         public Task<int> DeleteShopListAsync(ShopList slist)
         {
             return _database.DeleteAsync(slist);
+        }
+
+        public Task<int> SaveProductAsync(Product product)
+        {
+            if (product.ID != 0)
+            {
+                return _database.UpdateAsync(product);
+            }
+            else
+            {
+                return _database.InsertAsync(product);
+            }
+        }
+
+
+        public Task<int> DeleteProductAsync(Product product)
+        {
+            return _database.DeleteAsync(product);
+        }
+
+        public Task<List<Product>> GetProductsAsync()
+        {
+            return _database.Table<Product>().ToListAsync();
+        }
+
+
+        public Task<int> SaveListProductAsync(ListProduct listp)
+        {
+            if (listp.ID != 0)
+            {
+                return _database.UpdateAsync(listp);
+            }
+            else
+            {
+                return _database.InsertAsync(listp);
+            }
+        }
+
+        public Task<List<Product>> GetListProductsAsync(int shoplistid)
+        {
+            return _database.QueryAsync<Product>("select P.ID, P.Description from Product P" + " inner join ListProduct LP" + " on P.ID = LP.ProductID where LP.ShopListID = ?", shoplistid);
+
         }
     }
 }
--- ListPage.xaml.cs
+++ ListPage.xaml.cs
@@ -23,4 +23,42 @@ public partial class ListPage : ContentPage
         await App.Database.DeleteShopListAsync(slist);
         await Navigation.PopAsync();
     }
+
+    async void OnChooseButtonClicked(object sender, EventArgs e)
+    {
+
+        await Navigation.PushAsync(new ProductPage((ShopList)this.BindingContext)
+        {
+            BindingContext = new Product()
+        });
+
+    }
+    async void OnAddButtonClicked(object sender, EventArgs e)
+    {
+
+        Product p;
+        if (listView.SelectedItem != null)
+        {
+            p = listView.SelectedItem as Product;
+            var lp = new ListProduct()
+            {
+                ShopListID = sl.ID,
+                ProductID = p.ID
+            };
+            await App.Database.SaveListProductAsync(lp);
+            p.ListProducts = new List<ListProduct> { lp };
+
+            await Navigation.PopAsync();
+        }
+    }
+
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+
+        var shopl = (ShopList)BindingContext;
+
+        listView.ItemsSource = await App.Database.GetListProductsAsync(shopl.ID);
+    }
+
 }
